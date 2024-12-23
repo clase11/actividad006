9e07893caca6828bae012df5e0aa55c48909f819
unknown <oscarferrufino104@gmail.com>
2024-12-23 19:52:19 -0400

    commitactivida6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { CarFormComponent } from './car-form/car-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    CarFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/car-form/car-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car-form',
+  standalone: false,
+  
+  templateUrl: './car-form.component.html',
+  styleUrl: './car-form.component.css'
+})
+export class CarFormComponent {
+
+}
